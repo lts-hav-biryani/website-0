9121d56582dcb9f81ca33cc3624667adc45c3773
MahadevDoshetty <mahadevdoshetty317@gmail.com>
2026-05-08 01:16:55 +0530

    Commit 07

--- Backend/Controller/user/dashboard.controller.js
+++ Backend/Controller/user/dashboard.controller.js
@@ -1,6 +1,7 @@
 const { addressService, ordersService, issuesService, reviewService } = require("../../Services/User/dashboardFeatures");
 const { userInfo, userAddresses } = require("/Projects/LetsHavBiriyani-website/Backend/Models/user");
 const AppError = require("/Projects/LetsHavBiriyani-website/Backend/Utils/AppError");
+const bcrypt = require("bcrypt");
 const asyncHandler = require("/Projects/LetsHavBiriyani-website/Backend/Utils/AsyncHanlder");
 /**
  * @param {import("express").Request} req
@@ -38,4 +39,19 @@ const reviewsController = asyncHandler(async (req, res) => {
         status: true
     });
 });
+const passwordChangeController = asyncHandler(async (req, res) => {
+    const userId = req.user;
+    const { oldPassword, newPassword } = req.body;
+    const user = await userInfo.findById(_id);
+    const isMatch = await bcrypt.compare(oldPassword, user.password);
+    if (!isMatch) {
+        throw new AppError(400, "Invalid Old Password");
+    }
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    const updating = await user.updateOne({ _id: userId }, { password: hashedPassword });
+    return res.status(200).json({
+        message: "Password Changed successfully!",
+        status: true
+    })
+})
 module.exports = { addressController, orderController, reviewsController, issuesController }
