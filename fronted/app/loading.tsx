eb8361d5a6bff89620d03c26958520d7c4de49d5
MahadevDoshetty <mahadevdoshetty317@gmail.com>
2026-08-14 10:11:03 +0530

    Loading modified

--- fronted/app/loading.tsx
+++ fronted/app/loading.tsx
@@ -1,7 +1,15 @@
-export default function Loading() {
+import React from 'react'
+
+const loading = () => {
   return (
-    <div className="fixed inset-0 z-[9999] flex min-h-screen items-center justify-center bg-black/30 backdrop-blur-md">
-      <div className="h-12 w-12 animate-spin rounded-full border-4 border-gray-300 border-t-white" />
+    <div className="flex min-h-screen items-center justify-center bg-black">
+      <div className="relative h-12 w-12">
+        <div className="absolute inset-0 rounded-full border-4 border-gray-700"></div>
+
+        <div className="absolute inset-0 animate-spin rounded-full border-4 border-transparent border-t-amber-400"></div>
+      </div>
     </div>
   )
 }
+
+export default loading;
